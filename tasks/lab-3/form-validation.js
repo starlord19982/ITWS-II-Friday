1776b580fee70754e30e30e0b1777283afc52d50
Shivang Nagaria <shivang.nagaria@gmail.com>
2018-01-23 02:25:48 +0530

    auxillary function added

--- tasks/lab-3/form-validation.js
+++ tasks/lab-3/form-validation.js
@@ -1,11 +1,12 @@
-// Return true if input is valid
 
 function validateUsername(name) {
-	// Length of username should be between 4 to 8
-	// It should not contain any whitespace
+	//console.log(name.length);
+	if (name.length > 3 && name.length < 8)
+		return true;
+	return false;
 }
 
-function validatePassword(passowrd) {
+function validatePassword(password) {
 	// password should contain alteast 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special characters
 	// URL: https://account.cern.ch/account/Help/?kbid=020040
 }
@@ -23,7 +24,34 @@ function validateGender(gender) {
 	// Check if any one of radio button is selected
 }
 
-function validateLanguage(lang) {
+function validateLanguage(langs) {
 	// lang -> list of langs selected
 	// Check if atleast one of the checkbox is checked
 }
+
+
+// Auxillary function which parse input from the form fields
+function checkForm() {
+	// parsing username from the form
+	let name = document.getElementById("username").value;
+
+	if (validateUsername(name) /* && validatePassword()  && validateCountry(country) && validateEmail(email)
+			&& validateGender(gender) && validateLanguage(langs) */) {
+		// If all validations are successful, then enable the submit button
+		document.getElementById("submitbtn").disabled = false;
+		msg.innerHTML = "Form contains no errors";
+		console.log("Form contains no errors");
+	} else {
+		// If even one validation fails, disable submit button
+		document.getElementById("submitbtn").disabled = true;
+		msg.innerHTML = "Errors in the form";
+		console.log("Errors in the form");
+	}
+}
+
+// Run `checkForm` function on each and every keyup event
+document.getElementsByTagName("body")[0].onkeyup = function() {
+	checkForm();
+}
+
+checkForm();
